81447f7548c237495ba2798d95fdb32d510f904a
Saurabh Singh <sighsaurabh920@gmail.com>
2020-01-29 20:59:58 +0000

    command line

--- src/main/java/org/worldbuild/actuator/ActuatorApplication.java
+++ src/main/java/org/worldbuild/actuator/ActuatorApplication.java
@@ -1,13 +1,28 @@
 package org.worldbuild.actuator;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.core.env.Environment;
+@Log4j2
 @SpringBootApplication
-public class ActuatorApplication {
+public class ActuatorApplication implements CommandLineRunner {
+
+	@Autowired
+	private Environment environment;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActuatorApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("PUBLIC_IP - "+environment.getProperty("PUBLIC_IP"));
+		log.info("PUBLIC_IP - "+System.getProperty("PUBLIC_IP"));
+		log.info("PUBLIC_IP - "+System.getenv("PUBLIC_IP"));
+		log.info("ENV - "+System.getenv());
+
+	}
 }
